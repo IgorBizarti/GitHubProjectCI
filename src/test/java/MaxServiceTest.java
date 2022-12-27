b8961bacec5a3fa9541bd19e4fe715b25aed0beb
Igor Byazrov <fbi1978@mail.ru>
2022-12-27 16:21:51 +0300

    СhangedCommit

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,25 +1,18 @@
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaxServiceTest {
 
-
     @Test
+
     public void shouldFindMax() {
+        MaxService service = new MaxService();
+        int a = 5;
+        int b = 3;
 
-    MaxService service = new MaxService();
-
-    int a = 5;
-    int b =3;
-
-    int expected = 5;
-    int actual = service.max( a,b);
-
-        Assertions.assertEquals(expected, actual);
-
+        int expected = a;
+        int actual = service.max(a, b);
     }
-
 
 }
